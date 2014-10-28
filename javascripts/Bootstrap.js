1af2bce09f3f1c11281919bf6cc605b048339aac
NavyCo <Nicophillips@live.com>
2014-10-28 12:37:27 -0400

    Create Bootstrap.js

---
+++ javascripts/Bootstrap.js
@@ -0,0 +1,13 @@
+ 	var t1 = "<span>☰</span>";
+var t2 = "<nav>";
+var t3 = " <ul>";
+var t4 = " <li><li class='fa fa-refresh'></li></li>"
+var t5 = " </ul>";
+var t6 = "</nav>";
+$("#bootstrap").append(t1);
+$("#bootstrap").append(t2);
+$("#bootstrap").append(t3);
+$("#bootstrap").append(t4);
+$("#bootstrap").append(t5);
+$("#bootstrap").append(t6);
+// Make a div at the top named "bootstrap" (id)
