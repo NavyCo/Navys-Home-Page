ab48d67a939d379214e75a75f8fcfa754abb0543
NavyCo <Nicophillips@live.com>
2014-10-28 12:44:34 -0400

    Update Bootstrap.js

--- javascripts/Bootstrap.js
+++ javascripts/Bootstrap.js
@@ -8,16 +8,11 @@ $("span").click(function() {
     $(this).text("☰");
   }
 })
-var t1 = "<span>☰</span>";
-var t2 = "<nav>";
-var t3 = " <ul>";
-var t4 = " <li><li class='fa fa-refresh'></li></li>"
-var t5 = " </ul>";
-var t6 = "</nav>";
-$("#bootstrap").append(t1);
-$("#bootstrap").append(t2);
-$("#bootstrap").append(t3);
-$("#bootstrap").append(t4);
-$("#bootstrap").append(t5);
-$("#bootstrap").append(t6);
-// Make a div at the top named "bootstrap" (id)
+/* what your bootstrap should look like!
+<span>☰</span>
+	<nav>
+	 <ul>
+	 <li><a>Test</a></li>
+	 </ul>
+	</nav>
+*/
